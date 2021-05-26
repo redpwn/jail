5d97b61d4371c29c9a8a3db3405f3c61f495d7ec
Philip Papurt <ginkoid@gmail.com>
2021-05-26 10:43:29 -0400

    refactor: parse with github.com/caarlos0/env

--- go.mod
+++ go.mod
@@ -3,7 +3,8 @@ module github.com/redpwn/jail
 go 1.16
 
 require (
+	github.com/caarlos0/env/v6 v6.6.0
 	github.com/docker/go-units v0.4.0
-	golang.org/x/sys v0.0.0-20210510120138-977fb7262007
+	golang.org/x/sys v0.0.0-20210521090106-6ca3eb03dfc2
 	google.golang.org/protobuf v1.26.0
 )
--- run.go
+++ run.go
@@ -1,20 +1,20 @@
 package main
 
-//go:generate mkdir -p gen
-//go:generate protoc -Insjail --go_out gen --go_opt Mconfig.proto=/nsjail config.proto
+//go:generate mkdir -p proto
+//go:generate protoc -Insjail --go_out proto --go_opt Mconfig.proto=/nsjail config.proto
 
 import (
 	"bufio"
 	"fmt"
+	"github.com/caarlos0/env/v6"
 	"github.com/docker/go-units"
-	"github.com/redpwn/jail/gen/nsjail"
+	"github.com/redpwn/jail/proto/nsjail"
 	"golang.org/x/sys/unix"
 	"google.golang.org/protobuf/encoding/prototext"
 	"google.golang.org/protobuf/proto"
 	"os"
 	"os/exec"
 	"runtime"
-	"strconv"
 	"strings"
 )
 
@@ -38,21 +38,29 @@ type cgroupInfo struct {
 	cgroup2 bool
 }
 
+type size uint64
+
+func (s *size) UnmarshalText(t []byte) error {
+	v, err := units.RAMInBytes(string(t))
+	*s = size(v)
+	return err
+}
+
 type jailConfig struct {
-	time       uint32
-	conns      uint32
-	connsPerIp uint32
-	pids       uint64
-	mem        uint64
-	cpu        uint32
+	Time       uint32 `env:"JAIL_TIME" envDefault:"20"`
+	Conns      uint32 `env:"JAIL_CONNS"`
+	ConnsPerIp uint32 `env:"JAIL_CONNS_PER_IP"`
+	Pids       uint64 `env:"JAIL_PIDS" envDefault:"5"`
+	Mem        size   `env:"JAIL_MEM" envDefault:"5M"`
+	Cpu        uint32 `env:"JAIL_CPU" envDefault:"100"`
 	cgroup     cgroupInfo
 }
 
-func readCgroup() *cgroupInfo {
+func readCgroup() (*cgroupInfo, error) {
 	info := &cgroupInfo{}
 	f, err := os.Open("/proc/self/cgroup")
 	if err != nil {
-		panic(fmt.Errorf("read cgroup info: %w", err))
+		return nil, fmt.Errorf("read cgroup info: %w", err)
 	}
 	defer f.Close()
 	s := bufio.NewScanner(f)
@@ -74,70 +82,72 @@ func readCgroup() *cgroupInfo {
 	if info.pids == nil && info.mem == nil && info.cpu == nil {
 		info.cgroup2 = true
 	}
-	return info
+	return info, nil
 }
 
-func mountCgroup1(name string, entry *cgroup1Entry) {
+func mountCgroup1(name string, entry *cgroup1Entry) error {
 	dest := cgroupPath + "/" + name
 	if err := unix.Mount("none", dest, "cgroup", mountFlags, entry.controllers); err != nil {
-		panic(fmt.Errorf("mount cgroup1 %s to %s: %w", entry.controllers, dest, err))
+		return fmt.Errorf("mount cgroup1 %s to %s: %w", entry.controllers, dest, err)
 	}
 	if err := os.Chmod(dest, 0755); err != nil {
-		panic(err)
+		return err
 	}
 	delegated := dest + "/" + entry.parent
 	if err := os.Mkdir(delegated, 0755); err != nil {
-		panic(err)
+		return err
 	}
 	if err := os.Chown(delegated, nsjailId, nsjailId); err != nil {
-		panic(err)
+		return err
 	}
+	return nil
 }
 
-func mountCgroup2() {
+func mountCgroup2() error {
 	dest := cgroupPath + "/unified"
 	if err := unix.Mount("none", dest, "cgroup2", mountFlags, ""); err != nil {
-		panic(fmt.Errorf("mount cgroup2 to %s: %w", dest, err))
+		return fmt.Errorf("mount cgroup2 to %s: %w", dest, err)
 	}
 	jailPath := dest + "/jail"
 	if err := os.Mkdir(jailPath, 0700); err != nil {
-		panic(err)
+		return err
 	}
 	if err := os.WriteFile(jailPath+"/cgroup.procs", []byte("0"), 0); err != nil {
-		panic(err)
+		return err
 	}
 	if err := os.WriteFile(dest+"/cgroup.subtree_control", []byte("+pids +memory +cpu"), 0); err != nil {
-		panic(err)
+		return err
 	}
 	if err := os.Chown(dest+"/cgroup.procs", nsjailId, nsjailId); err != nil {
-		panic(fmt.Errorf("cgroup2 delegate: %w", err))
+		return fmt.Errorf("cgroup2 delegate: %w", err)
 	}
 	runPath := dest + "/run"
 	if err := os.Mkdir(runPath, 0700); err != nil {
-		panic(err)
+		return err
 	}
 	if err := os.WriteFile(runPath+"/cgroup.subtree_control", []byte("+pids +memory +cpu"), 0); err != nil {
-		panic(err)
+		return err
 	}
 	if err := os.Chown(runPath, nsjailId, nsjailId); err != nil {
-		panic(err)
+		return err
 	}
+	return nil
 }
 
-func writeConfig(cfg *jailConfig) {
-	m := &nsjail.NsJailConfig{
+func writeConfig(cfg *jailConfig) error {
+	msg := &nsjail.NsJailConfig{
 		Mode:              nsjail.Mode_LISTEN.Enum(),
 		Port:              proto.Uint32(5000),
-		TimeLimit:         &cfg.time,
-		MaxConns:          &cfg.conns,
-		MaxConnsPerIp:     &cfg.connsPerIp,
+		TimeLimit:         &cfg.Time,
+		MaxConns:          &cfg.Conns,
+		MaxConnsPerIp:     &cfg.ConnsPerIp,
 		RlimitAsType:      nsjail.RLimit_HARD.Enum(),
 		RlimitCpuType:     nsjail.RLimit_HARD.Enum(),
 		RlimitFsizeType:   nsjail.RLimit_HARD.Enum(),
 		RlimitNofileType:  nsjail.RLimit_HARD.Enum(),
-		CgroupPidsMax:     &cfg.pids,
-		CgroupMemMax:      &cfg.mem,
-		CgroupCpuMsPerSec: &cfg.cpu,
+		CgroupPidsMax:     &cfg.Pids,
+		CgroupMemMax:      proto.Uint64(uint64(cfg.Mem)),
+		CgroupCpuMsPerSec: &cfg.Cpu,
 		SeccompString: []string{`
 			ERRNO(1) {
 				clone { (clone_flags & 0x7e020000) != 0 },
@@ -159,88 +169,70 @@ func writeConfig(cfg *jailConfig) {
 		},
 	}
 	if cfg.cgroup.cgroup2 {
-		m.UseCgroupv2 = proto.Bool(true)
-		m.Cgroupv2Mount = proto.String(cgroupPath + "/unified/run")
+		msg.UseCgroupv2 = proto.Bool(true)
+		msg.Cgroupv2Mount = proto.String(cgroupPath + "/unified/run")
 	} else {
-		m.CgroupPidsMount = proto.String(cgroupPath + "/pids")
-		m.CgroupPidsParent = &cfg.cgroup.pids.parent
-		m.CgroupMemMount = proto.String(cgroupPath + "/mem")
-		m.CgroupMemParent = &cfg.cgroup.mem.parent
-		m.CgroupCpuMount = proto.String(cgroupPath + "/cpu")
-		m.CgroupCpuParent = &cfg.cgroup.cpu.parent
+		msg.CgroupPidsMount = proto.String(cgroupPath + "/pids")
+		msg.CgroupPidsParent = &cfg.cgroup.pids.parent
+		msg.CgroupMemMount = proto.String(cgroupPath + "/mem")
+		msg.CgroupMemParent = &cfg.cgroup.mem.parent
+		msg.CgroupCpuMount = proto.String(cgroupPath + "/cpu")
+		msg.CgroupCpuParent = &cfg.cgroup.cpu.parent
 	}
-	c, err := prototext.Marshal(m)
+	content, err := prototext.Marshal(msg)
 	if err != nil {
-		panic(err)
+		return err
 	}
-	if err := os.WriteFile(nsjailCfgPath, c, 0644); err != nil {
-		panic(err)
+	if err := os.WriteFile(nsjailCfgPath, content, 0644); err != nil {
+		return err
 	}
+	return nil
 }
 
-func runNsjail() {
+func runNsjail() error {
 	runtime.LockOSThread()
 	defer runtime.UnlockOSThread()
 	if err := unix.Setresgid(nsjailId, nsjailId, nsjailId); err != nil {
-		panic(fmt.Errorf("setresgid nsjail: %w", err))
+		return fmt.Errorf("setresgid nsjail: %w", err)
 	}
 	if err := unix.Setgroups([]int{nsjailId}); err != nil {
-		panic(fmt.Errorf("setgroups nsjail: %w", err))
+		return fmt.Errorf("setgroups nsjail: %w", err)
 	}
 	if err := unix.Setresuid(nsjailId, nsjailId, nsjailId); err != nil {
-		panic(fmt.Errorf("setresuid nsjail: %w", err))
+		return fmt.Errorf("setresuid nsjail: %w", err)
 	}
 	capHeader := &unix.CapUserHeader{Version: unix.LINUX_CAPABILITY_VERSION_3}
 	// https://github.com/golang/go/issues/44312
 	capData := [2]unix.CapUserData{}
 	if err := unix.Capset(capHeader, &capData[0]); err != nil {
-		panic(fmt.Errorf("capset: %w", err))
+		return fmt.Errorf("capset: %w", err)
 	}
 	if err := unix.Exec("/jail/nsjail", []string{"nsjail", "-C", nsjailCfgPath}, os.Environ()); err != nil {
-		panic(fmt.Errorf("exec nsjail: %w", err))
+		return fmt.Errorf("exec nsjail: %w", err)
 	}
+	return nil
 }
 
-func readEnv(key string, convert func(string) (uint64, error), fallback string) uint64 {
-	env := os.Getenv(key)
-	if env == "" {
-		env = fallback
-	}
-	val, err := convert(env)
-	if err != nil {
-		panic(fmt.Errorf("read env %s: %w", key, err))
-	}
-	return val
-}
-
-func convertNum(s string) (uint64, error) {
-	val, err := strconv.Atoi(s)
-	return uint64(val), err
-}
-
-func convertSize(s string) (uint64, error) {
-	val, err := units.RAMInBytes(s)
-	return uint64(val), err
-}
-
-func mountTmp() {
+func mountTmp() error {
 	if err := unix.Mount("none", "/tmp", "tmpfs", mountFlags, ""); err != nil {
-		panic(fmt.Errorf("mount tmpfs: %w", err))
+		return fmt.Errorf("mount tmpfs: %w", err)
 	}
+	return nil
 }
 
-func mountDev() {
+func mountDev() error {
 	if _, err := os.Stat("/srv/dev"); os.IsNotExist(err) {
-		return
+		return nil
 	}
 	if err := unix.Mount("/jail/dev", "/srv/dev", "none", mountFlags|unix.MS_BIND, ""); err != nil {
-		panic(fmt.Errorf("mount dev: %w", err))
+		return fmt.Errorf("mount dev: %w", err)
 	}
+	return nil
 }
 
-func runHook() {
+func runHook() error {
 	if _, err := os.Stat(hookPath); os.IsNotExist(err) {
-		return
+		return nil
 	}
 	cmd := exec.Command("/bin/sh", hookPath)
 	cmd.Env = append(
@@ -251,30 +243,56 @@ func runHook() {
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
 	if err := cmd.Run(); err != nil {
-		panic(fmt.Errorf("exec hook: %w", err))
+		return fmt.Errorf("exec hook: %w", err)
 	}
+	return nil
+}
+
+func run() error {
+	if err := mountTmp(); err != nil {
+		return err
+	}
+	if err := mountDev(); err != nil {
+		return err
+	}
+	cgroup, err := readCgroup()
+	if err != nil {
+		return err
+	}
+	if cgroup.cgroup2 {
+		if err := mountCgroup2(); err != nil {
+			return err
+		}
+	} else {
+		if err := mountCgroup1("pids", cgroup.pids); err != nil {
+			return err
+		}
+		if err := mountCgroup1("mem", cgroup.mem); err != nil {
+			return err
+		}
+		if err := mountCgroup1("cpu", cgroup.cpu); err != nil {
+			return err
+		}
+	}
+	cfg := &jailConfig{cgroup: *cgroup}
+	if err := env.Parse(cfg); err != nil {
+		return err
+	}
+	if err := writeConfig(cfg); err != nil {
+		return err
+	}
+	if err := runHook(); err != nil {
+		return err
+	}
+	if err := runNsjail(); err != nil {
+		return err
+	}
+	return nil
 }
 
 func main() {
-	mountTmp()
-	mountDev()
-	cgroup := readCgroup()
-	if cgroup.cgroup2 {
-		mountCgroup2()
-	} else {
-		mountCgroup1("pids", cgroup.pids)
-		mountCgroup1("mem", cgroup.mem)
-		mountCgroup1("cpu", cgroup.cpu)
+	if err := run(); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
 	}
-	writeConfig(&jailConfig{
-		time:       uint32(readEnv("JAIL_TIME", convertNum, "30")),
-		conns:      uint32(readEnv("JAIL_CONNS", convertNum, "0")),
-		connsPerIp: uint32(readEnv("JAIL_CONNS_PER_IP", convertNum, "0")),
-		pids:       readEnv("JAIL_PIDS", convertNum, "5"),
-		mem:        readEnv("JAIL_MEM", convertSize, "5M"),
-		cpu:        uint32(readEnv("JAIL_CPU", convertNum, "100")),
-		cgroup:     *cgroup,
-	})
-	runHook()
-	runNsjail()
 }
