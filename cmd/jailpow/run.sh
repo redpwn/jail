c6af2d34e81da6c375cd648cea2ea1ddacec41c9
Philip Papurt <ginkoid@gmail.com>
2021-06-18 16:52:16 -0400

    feat: add os selection to run.sh

--- cmd/jailpow/run.sh
+++ cmd/jailpow/run.sh
@@ -1,11 +1,22 @@
 #!/bin/sh
+# jailpow runner
+# https://github.com/redpwn/jail/blob/master/cmd/jailpow/run.sh
 
 set -e
-
-version=v0.0.1
-
-cache_root=~/.cache/jailpow
-mkdir -p "$cache_root"
-cache_path="$cache_root/$version"
-[ -e "$cache_path" ] || curl -sSfLo "$cache_path" "https://github.com/redpwn/jail/releases/download/$version/jailpow-linux-amd64" && chmod u+x "$cache_path"
-"$cache_path" "$1"
+version=v0.0.2
+challenge=$1
+run() {
+  cache_root=$HOME/.cache/jailpow
+  mkdir -p "$cache_root"
+  cache_path="$cache_root/jailpow-$version"
+  case $(uname | tr '[:upper:]' '[:lower:]') in
+    linux*) release=linux-amd64;;
+    darwin*) release=darwin-amd64;;
+    msys*) release=windows-amd64.exe;;
+    cygwin*) release=windows-amd64.exe;;
+    *) echo unknown OS; exit 1
+  esac
+  [ -e "$cache_path" ] || curl -sSfLo "$cache_path" "https://github.com/redpwn/jail/releases/download/$version/jailpow-$release" && chmod u+x "$cache_path"
+  "$cache_path" "$challenge"
+}
+run
